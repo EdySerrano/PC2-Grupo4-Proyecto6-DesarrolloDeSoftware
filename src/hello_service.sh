3e40680ebc0639d3c935bd4c2b3e382fc2498a01
Edy-Serrano <edy.serrano.a@uni.pe>
2025-09-24 11:34:42 -0500

    Implementacion de netcat para respuestas HTTP

--- src/hello_service.sh
+++ src/hello_service.sh
@@ -5,3 +5,9 @@ PORT="${PORT:-8080}"
 APP_ENV="${APP_ENV:-dev}"
 
 trap 'echo "Servidor detenido"; exit 0' SIGINT SIGTERM
+
+# Servidor HTTP simple usando netcat
+while true; do
+  { echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nsalud OK - $APP_ENV"; } \
+    | nc -l -p "$PORT" -q 1
+done
