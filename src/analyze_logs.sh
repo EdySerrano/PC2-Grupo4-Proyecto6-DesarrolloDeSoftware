35851f46a14e1d91a1b23902431ee4be629f0d54
GermainR24 <germainronald23@gmail.com>
2025-09-28 09:52:58 -0500

    Implementar analisis de latencia

--- src/analyze_logs.sh
+++ src/analyze_logs.sh
@@ -17,3 +17,29 @@ trap cleanup SIGINT SIGTERM
 mkdir -p "$OUTPUT_DIR"
 
 log "Iniciando el analisis de logs del servicio Hello"
+
+# Analizar latencias usando herramientas Unix
+analyze_latencies() {
+    log "Analizando latencias"
+    
+    grep "latency:" "$LOGFILE" 2>/dev/null | \
+        sed 's/.*latency: \([0-9]*\)ms.*/\1/' | \
+        awk 'BEGIN {sum=0; count=0; max=0; min=999999} 
+             {
+                sum+=$1; 
+                count++; 
+                if($1>max) max=$1; 
+                if($1<min) min=$1
+             } 
+             END {
+                if(count>0) {
+                    avg=sum/count;
+                    print "Latencia promedio:", avg "ms";
+                    print "Latencia maxima:", max "ms"; 
+                    print "Latencia minima:", min "ms";
+                    print "Total requests:", count
+                } else {
+                    print "No se encontraron datos de latencia"
+                }
+             }' | tee "$OUTPUT_DIR/latency_analysis.txt"
+}
